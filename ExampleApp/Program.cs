c3eafdc4eb91e924903896d2e8c78c22711e48a8
Terraxel99 <thomas.dudziak@outlook.be>
2024-02-01 18:54:38 +0100

    Removed old functions and using only safe-executed code

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -10,7 +10,7 @@ try
     var temporals = new List<TemporalGeometryPoint>()
     {
         TemporalGeometryPoint.From("[POINT(1 5)@2021-05-02, POINT(12 2)@2021-06-02]"),
-        TemporalGeometryPoint.From("ePOINT(11 3)@2023-08-06 01:45:00+00:00"),
+        TemporalGeometryPoint.From("POINT(11 3)@2023-08-06 01:45:00+00:00"),
         TemporalGeometryPoint.From("[POINT(35 12)@2023-01-01, POINT(36 14)@2023-01-02]"),
     };
 
@@ -26,10 +26,12 @@ try
     // var tempSeqSet = TemporalGeometryPoint.From("Interp=Step;{[POINT(1 1)@2000-01-01, POINT(2 2)@2000-01-02],[POINT(3 3)@2000-01-03, POINT(3 3)@2000-01-04]}");
     // var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "json_output.json");
     // File.WriteAllText(path, tempSeqSet.ToJson());
+
+    var erroneousInput = TemporalGeometryPoint.From("e[POINT(1 5)@2021-05-02, POINT(12 2)@2021-06-02]");
 }
 catch (MEOSException e)
 {
-    Console.WriteLine($"Could catch the exception : {e.Code} - {e.Message}");
+    Console.WriteLine($"An error occurred in MEOS : {e.Code} - {e.Message}");
 }
 finally
 {
--- MEOS.NET/Lifecycle/MEOSLifecycle.cs
+++ MEOS.NET/Lifecycle/MEOSLifecycle.cs
@@ -6,11 +6,9 @@ namespace MEOS.NET.Lifecycle
     public static class MEOSLifecycle
     {
         public static void Initialize(string timeZone)
-        {
-            MEOSFunctions.meos_initialize(timeZone, MEOSErrorHandling.InternalErrorHandler);
-        }
+            => MEOSExposedFunctions.meos_initialize(timeZone, MEOSErrorHandling.InternalErrorHandler);
 
         public static void Terminate()
-            => MEOSFunctions.meos_finalize(); 
+            => MEOSExposedFunctions.meos_finalize(); 
     }
 }
