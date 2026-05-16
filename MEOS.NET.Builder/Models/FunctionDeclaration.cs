043c058c4a829fb8bbe1bb5ec123df408abaeeed
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-05-16 23:50:59 +0200

    Drop MEOS.NET.Builder; tools/codegen.py replaces it

--- MEOS.NET.Builder/Models/FunctionDeclaration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MEOS.NET.Builder.Models
-{
-	internal abstract class FunctionDeclaration
-	{
-        internal string? ReturnType { get; init; } = string.Empty;
-        internal string? FunctionName { get; init; } = string.Empty;
-    }
-}
-
